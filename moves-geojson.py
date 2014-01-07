349a30a2be5984bb50e78c59b3e11182bec173f4
Paul Mison <paul@bynd.com>
2014-01-06 17:32:28 -0800

    Templates\! CSS\! A list of days\! All sorts of good stuff\!

--- moves-geojson.py
+++ moves-geojson.py
@@ -1,9 +1,9 @@
 import os
 
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from json import dumps
 
-from flask import Flask, url_for, request, session, redirect
+from flask import Flask, url_for, request, session, redirect, render_template
 from moves import MovesClient
 
 app = Flask(__name__)
@@ -18,12 +18,15 @@ Moves = MovesClient(client_id, client_secret)
 @app.route("/")
 def index():
     if 'token' not in session:
-        oauth_return_url = url_for('oauth_return', _external=True)
-        auth_url = Moves.build_oauth_url(oauth_return_url)
-        return 'Authorize this application: <a href="%s">%s</a>' % \
-            (auth_url, auth_url)
-    return redirect(url_for('geojson'))
+        return render_template("index.html")
 
+    return redirect(url_for('list'))
+
+@app.route("/auth")
+def auth():
+    oauth_return_url = url_for('oauth_return', _external=True)
+    auth_url = Moves.build_oauth_url(oauth_return_url)
+    return redirect(auth_url)
 
 @app.route("/oauth_return")
 def oauth_return():
@@ -34,8 +37,7 @@ def oauth_return():
     code = request.args.get("code")
     token = Moves.get_oauth_token(code, redirect_uri=oauth_return_url)
     session['token'] = token
-    return redirect(url_for('show_info'))
-
+    return redirect(url_for('list'))
 
 @app.route('/logout')
 def logout():
@@ -43,9 +45,27 @@ def logout():
         del(session['token'])
     return redirect(url_for('index'))
 
+@app.route('/list')
+def list():
+    if 'token' not in session:
+        return redirect(url_for('index'))
+
+    profile = Moves.user_profile(access_token=session['token'])
+
+    days = get_dates_range(profile['profile']['firstDate'])
+
+    return render_template("list.html", profile=profile, days=days)
+
+#     response = 'User ID: %s<br />First day using Moves: %s' % \
+#         (profile['userId'], profile['profile']['firstDate'])
+#     return response + "<br /><a href=\"%s\">Info for today</a>" % url_for('today') + \
+#         "<br /><a href=\"%s\">Logout</a>" % url_for('logout')
 
 @app.route("/info")
 def show_info():
+    if 'token' not in session:
+        return redirect(url_for('index'))
+
     profile = Moves.user_profile(access_token=session['token'])
     response = 'User ID: %s<br />First day using Moves: %s' % \
         (profile['userId'], profile['profile']['firstDate'])
@@ -53,13 +73,13 @@ def show_info():
         "<br /><a href=\"%s\">Logout</a>" % url_for('logout')
 
 
-@app.route("/geojson")
-def geojson():
+@app.route("/geojson/<date>")
+def geojson(date):
     if 'token' not in session:
-        return redirect("/")
+        return redirect(url_for('index'))
 
-    today = datetime.now().strftime('%Y%m%d')
-    info = Moves.user_storyline_daily(today, trackPoints={'true'}, access_token=session['token'])
+    api_date = date.replace('-', '')
+    info = Moves.user_storyline_daily(api_date, trackPoints={'true'}, access_token=session['token'])
     
     features = []
     
@@ -73,6 +93,28 @@ def geojson():
     geojson = {'type': 'FeatureCollection', 'features': features}
 
     return dumps(geojson)
+
+### utilities
+
+def get_dates_range(firstDate):
+    year = int(firstDate[0:4])
+    month = int(firstDate[4:6])
+    day = int(firstDate[6:8])
+
+    first = date(year, month, day)
+
+    now = datetime.now()
+    today = date(now.year, now.month, now.day)
+
+    days = []
+    cursor = today
+    
+    while cursor >= first:
+        days.append(cursor)
+        cursor = cursor - timedelta(days=1)
+
+    return days
+
 
 def geojson_move(segment):
     features = []
